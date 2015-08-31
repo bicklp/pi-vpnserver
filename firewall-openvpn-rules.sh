e0b1617e2a466501b249a9ab4513e8ec3567689a
bicklp <bicklp@gmail.com>
2015-08-31 22:19:53 +0100

    Update firewall-openvpn-rules.sh

--- firewall-openvpn-rules.sh
+++ firewall-openvpn-rules.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o [name of LAN/WLAN interface] -j SNAT --to-source 192.168.x.x #[Local IP of VPN Server]
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o [name of LAN/WLAN interface] -j SNAT --to-source 192.168.1.20 #[Local IP of VPN Server]
